a18dcc46d122e9056dc4f0644e45d0214053852d
Nidhi Verma <59047173+Nidd1995@users.noreply.github.com>
2020-09-20 17:42:11 +0530

    Create 3SUM.java

---
+++ 3SUM.java
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/3sum/
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+        List<List<Integer>> res = new ArrayList();
+      int n= nums.length;
+        if(n<3)
+        {
+            return new ArrayList();
+        }
+        Arrays.sort(nums);
+        for(int i=0; i<n-2 ; i++){
+          if(i==0 || nums[i]!= nums[i-1]){
+                  int j =i+1;
+            int k=n-1;
+            while(j<k)
+            {
+                int sum = nums[i];
+                sum = sum+ +nums[j];
+                sum= sum+nums[k];
+                
+                if(sum==0){
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    while(j<k && nums[j]==nums[j+1]) j++;
+                    while(j<k && nums[k]== nums[k-1])k--;
+                      j++;
+                    k--;
+                    
+                }else if(sum <0)
+                    j++;
+                else
+                    k--;
+                
+            }
+            }
+            
+        }
+       return res; 
+    }
+}
